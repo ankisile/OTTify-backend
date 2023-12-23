95c1baa374c8368f8d4635c04d1bf532883fdfad
siss-ankisile <ankisile@gmail.com>
2023-12-24 01:50:58 +0900

    Feat : 헤더의 accessToken으로 부터 email 추출 (#6)

--- src/main/java/tavebalak/OTTify/oauth/jwt/JwtService.java
+++ src/main/java/tavebalak/OTTify/oauth/jwt/JwtService.java
@@ -134,11 +134,13 @@ public class JwtService {
     public Optional<String> extractEmail(String accessToken) {
         try {
             // 토큰 유효성 검사하는 데에 사용할 알고리즘이 있는 JWT verifier builder 반환
-            return Optional.ofNullable(JWT.require(Algorithm.HMAC512(secretKey))
+            Optional<String> email = Optional.ofNullable(JWT.require(Algorithm.HMAC512(secretKey))
                     .build() // 반환된 빌더로 JWT verifier 생성
                     .verify(accessToken) // accessToken을 검증하고 유효하지 않다면 예외 발생
                     .getClaim(EMAIL_CLAIM) // claim(Emial) 가져오기
                     .asString());
+            log.info(email.get());
+            return email;
         } catch (Exception e) {
             log.error("액세스 토큰이 유효하지 않습니다.");
             return Optional.empty();
